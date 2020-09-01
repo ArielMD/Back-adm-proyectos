aa1adb28d9154b4ba9cd7f112bff9f3d4553260e
Ariel May <aemd199711@gmail.com>
2020-08-31 23:03:56 -0500

    fix: configurar cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const conectionDB = require("./config/database");
 const routes = require("./api/routes");
+const cors = require("cors");
 
 app = express();
 
 conectionDB();
+
+app.use(cors());
 app.use(express.json({ extended: true }));
 port = process.env.PORT || 4000;
 
